0217f9daf406262ac047d2dac0be578e64f5bf6a
Jenny Song <jenny.jieun.song@gmail.com>
2017-01-21 12:31:13 -0800

    jen3

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
 	socket.emit('connected')
-  socket.on('user join', function(oUser){
 
+  socket.on('user join', function(oUser){
     var user = new App.Model.User({
       userName : oUser.userName,
       avatarId : oUser.avatarId,
@@ -27,24 +27,29 @@ io.on('connection', function(socket){
       roomId: oUser.roomId,
       id : socket.id
     });
-
     var room = rooms.getOrInit(oUser.roomId);
     room.addUser(user);
     allUsers.add(user);
-  	
-    //join the user to the socket gameRoom
     socket.join(oUser.roomId);
 
   	io.to(oUser.roomId).emit('user joined', room.users.toJSON());
     console.log(room.users.toJSON());
   });
 
+  var countDown;
   socket.on('user is ready', function(){
     var user = allUsers.get(socket.id);
     var room = rooms.getOrInit(user.get('roomId'));
     user.set('userStatus', true);
     io.to(user.get('roomId')).emit('ready status', room.users.toJSON());
-    console.log(room.users.toJSON());
+
+    if (_everyoneReady(room.users)){
+      console.log("countdown start!");
+      countDown = setTimeout(function(){
+        io.to(user.get('roomId')).emit('game start');
+        console.log("game start!");
+      }, 10000)
+    }
 
   })
   socket.on('user is not ready', function(){
@@ -52,10 +57,18 @@ io.on('connection', function(socket){
     var room = rooms.getOrInit(user.get('roomId'));
     user.set('userStatus', false);
     io.to(user.get('roomId')).emit('ready status', room.users.toJSON());
-    console.log(room.users.toJSON());
+    clearTimeout(countDown);
+    console.log("can't start game yet!");
   })
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+var _everyoneReady = function(users){
+  return users.every(function(user){
+    return user.get('userStatus');
+  });
+
+}
